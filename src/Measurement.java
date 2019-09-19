2b8d87130499d721b29c44bc2e9ac821776fb511
Sjoerd Jakobs <sjakobs@chello.nl>
2019-09-19 15:37:48 +0200

    the gitignore file has been changed and slight changes in code  after the feedback from johan

--- src/Measurement.java
+++ src/Measurement.java
@@ -1,4 +1,19 @@
-public class Measurement {
+public class Measurement
+{
+
+    private double barometer;
+    private double insideTemp;
+    private double insideHum;
+    private double outsideTemp;
+    private double windSpeed;
+    private double avgWindSpeed;
+    private double windDir;
+    private double outsideHum;
+    private double rainRate;
+    private double uvLevel;
+    private double battLevel;
+    private String sunSet;
+    private String sunRise;
 
     public Measurement()
     {
@@ -7,36 +22,60 @@ public class Measurement {
 
     public Measurement(RawMeasurement rawData)
     {
-
+       setBarometer(rawData.getBarometer());
+       setInsideTemp(rawData.getInsideTemp());
     }
 
-    private double barometer;
     public void setBarometer (short val) { this.barometer = ValueConverter.airPressure(val);};
     public double getBarometer () { return barometer; };
 
-    //public double Barometer = ValueConverter.airPressure(rawData.getBarometer());
-    //public double insideTemperature = ValueConverter.temperature(rawData.getInsideTemp());
-    //public double insideHum = ValueConverter.humidity(rawData.getInsideHum());
-    //public double outsideTemperature = ValueConverter.temperature(rawData.getOutsideTemp());
-    //public double windspeed = ValueConverter.windSpeed(rawData.getWindSpeed());
-    //public double avgWindSpeed = ValueConverter.windSpeed(rawData.getWindSpeed());
-    //public double windDir = ValueConverter.windDirection(rawData.getWindDir());
-    //public double outsideHum = ValueConverter.humidity(rawData.getOutsideHum());
-    //public double rainRate = ValueConverter.rainMeter(rawData.getRainRate());
-    //public double uvLevel = ValueConverter.uvIndex(rawData.getUVLevel());
-    // public double solarRad = ValueConverter.;
-    //public double xmitBatt = rawData.getXmitBatt();
-    //public double battLevel = ValueConverter.batteryLevel(rawData.getBattLevel());
-    // public double foreIcon = ;
-    //public String sunRise = ValueConverter.sunRise(rawData.getSunrise());
-    //public String sunSet = ValueConverter.sunSet(rawData.getSunset());
+    public void setInsideTemp (short val) { this.insideTemp = ValueConverter.temperature(val);};
+    public double getInsideTemp () { return insideTemp; };
 
-    /*
+    public void setInsideHum (short val) { this.insideHum = ValueConverter.humidity(val);};
+    public double getInsideHum () { return insideHum; };
+
+    public void setOutsideTemp (short val) { this.outsideTemp = ValueConverter.temperature(val);};
+    public double getOutsideTemp () { return outsideTemp; };
+
+    public void setWindSpeed (short val) { this.windSpeed = ValueConverter.windSpeed(val);};
+    public double getWindSpeed () { return windSpeed; };
+
+    public void setAvgWindSpeed (short val) { this.avgWindSpeed = ValueConverter.windSpeed(val);};
+    public double getAvgWindSpeed () { return avgWindSpeed; };
+
+    public void setWindDir (short val) { this.windDir = ValueConverter.windDirection(val);};
+    public double getWindDir () { return windDir; };
+
+    public void setOutsideHum (short val) { this.outsideHum = ValueConverter.humidity(val);};
+    public double getOutsideHum () { return outsideHum; };
+
+    public void setRainRate (short val) { this.rainRate = ValueConverter.rainMeter(val);};
+    public double getRainRate () { return rainRate; };
+
+    public void setUvLevel (short val) { this.uvLevel = ValueConverter.uvIndex(val);};
+    public double getUvLevel () { return uvLevel; };
+
+    //solar rad
+
+    //xmittbat
+
+    public void setBattLevel (short val) { this.battLevel = ValueConverter.batteryLevel(val);};
+    public double getBattLevel () { return battLevel; };
+
+    //ForeIcon
+    public void SetSunSet (short val) { this.sunSet = ValueConverter.sunSet(val);};
+    public String GetSunSet () { return sunSet; };
+
+    public void SetSunRise (short val) { this.sunRise = ValueConverter.sunRise(val);};
+    public String GetSunRise () { return sunRise; };
+
+
     public String toString()
     {
         String s = "RawMeasurement:"
-                + "\nstationId = \t" + stationId
-                + "\ndateStamp = \t" + dateStamp
+                /*+ "\nstationId = \t" + stationId
+                + "\ndateStamp = \t" + dateStamp*/
                 + "\nbarometer = \t" + barometer
                 + "\ninsideTemp = \t" + insideTemp
                 + "\ninsideHum = \t" + insideHum
@@ -46,15 +85,13 @@ public class Measurement {
                 + "\nwindDir = \t" + windDir
                 + "\noutsideHum = \t" + outsideHum
                 + "\nrainRate = \t" + rainRate
-                + "\nUVLevel = \t" + UVLevel
+                /*+ "\nUVLevel = \t" + UVLevel
                 + "\nsolarRad = \t" + solarRad
-                + "\nxmitBatt = \t" + xmitBatt
-                + "\nbattLevel = \t" + battLevel
-                + "\nforeIcon = \t" + foreIcon
+                + "\nxmitBatt = \t" + xmitBatt*/
+                + "\nbattLevel = \t" + battLevel;
+                /*+ "\nforeIcon = \t" + foreIcon
                 + "\nsunrise = \t" + sunrise
-                + "\nsunset = \t" + sunset;
+                + "\nsunset = \t" + sunset;*/
         return s;
-    }*/
-
-
+    }
 }
