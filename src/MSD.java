9ad251377154f200681fc360c136bcae12292def
Samirr26 <samir.rademakers2@gmail.com>
2019-10-03 17:49:21 +0100

    no message

---
+++ src/MSD.java
@@ -0,0 +1,104 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static java.lang.Math.sqrt;
+
+public class MSD {
+    /**
+     * calculate the mode (most frequent number in the array)
+     *
+     */
+    public static double mode(ArrayList<Double> array){
+        double most = 0;
+        int mostCount = 0;
+
+        for(int i = 0; i < array.size(); i++){
+            int count = 0;
+
+            for(int j = 0; j < array.size(); j++){
+                if (array.get(j) == array.get(i)){
+                    count++;
+                }
+
+                if ( count > mostCount){
+                    mostCount = count;
+                    most = array.get(i);
+                }
+            }
+        }
+        return most;
+    }
+
+    /**
+     *
+     * calculate the median (the middle value of a sorted array)
+     */
+
+    public static double median(ArrayList<Double> array){
+        double[] sorted = new double[array.size()];
+
+        System.arraycopy(array, 0, sorted,0,array.size());
+        Arrays.sort(sorted);
+
+        if (array.size() % 2 ==0){
+            return (sorted[(sorted.length / 2) - 1] + sorted[sorted.length / 2]) / 2;
+        } else {
+            return sorted[sorted.length / 2];
+        }
+    }
+
+    /**
+     *
+     * calculate the mean
+     */
+    public static double mean(ArrayList<Double> array){
+        double sum = 0;
+        double average = 0;
+
+        for (int i = 0; i < array.size(); i++){
+            sum = sum + array.get(i);
+            average = sum / array.size();
+        }
+        return average;
+    }
+
+    /**
+     *
+     * calculate the standard deviation
+     */
+    public static double standardDeviation(ArrayList<Double> array){
+        double sd = 0;
+        for (int i = 0; i < array.size(); i++){
+            sd = sd + ((sqrt((array.get(i) - mean(array))* (array.get(i) - mean(array))))/ (array.size() - 1));
+        }
+        return sd;
+    }
+
+    /**
+     *
+     * calculate the minimum
+     */
+    public static double minimum(ArrayList<Double> array){
+        double minimum = array.get(0);
+        for (int i = 0; i < array.size(); i++){
+            if (array.get(i) < minimum){
+                minimum = array.get(i);
+            }
+        }
+        return minimum;
+    }
+
+    /**
+     *
+     * calculate the maximum
+     */
+    public static double maximum(ArrayList<Double> array){
+        double maximum = 0;
+        for (int i = 0; i < array.size(); i++){
+            if (array.get(i) > maximum){
+                maximum = array.get(i);
+            }
+        }
+        return maximum;
+    }
+}
--- src/main.java
+++ src/main.java
@@ -17,30 +17,37 @@ public class main
         /**
          * get last temperature, put it on the display and print
          */
-        RawMeasurement rawData = DatabaseConnection.getMostRecentMeasurement();
-        Measurement thisAfternoon =  new Measurement(rawData);
+//        RawMeasurement rawData = DatabaseConnection.getMostRecentMeasurement();
+//        Measurement thisAfternoon =  new Measurement(rawData);
 
-        System.out.println(thisAfternoon.getOutsideTemp());
-
-        String strDouble = String.format("%.2f", thisAfternoon.getOutsideTemp());
-        HelperFunctions.WriteOnMatrixScreen("OutsideTemp " + strDouble + "    bovenste value is ooktemperatuur");
-        HelperFunctions.WriteValueOnSegments(1,thisAfternoon.getOutsideTemp(), 3);
+//        System.out.println(thisAfternoon.getOutsideTemp());
 
 
         /**
-        * get the mean of temperatures from the last hour
-        */
-        ArrayList<RawMeasurement> rawDatas = DatabaseConnection.getMeasurementsLastHour();
-        ArrayList<Measurement>  measurements = new ArrayList<Measurement>();
+         * get the mean of temperatures from the last hour
+//         */
+        ArrayList<RawMeasurement> rawDatas = DatabaseConnection.getMeasurementsLastDays(1);
+        ArrayList<Measurement> measurements = new ArrayList<Measurement>();
+        ArrayList<Double> temperature = new ArrayList<>();
+//        ArrayList<Double> maxTemp = new ArrayList<>();
+//        ArrayList<Double> minTemp = new ArrayList<>();
 
-        Double mean = 0.0;
         for (int i = 0; i < rawDatas.size(); i++)
         {
             measurements.add(new Measurement(rawDatas.get(i)));
-            mean += measurements.get(i).getOutsideTemp();
+            temperature.add(measurements.get(i).getOutsideTemp());
+//            measurements.get(1).getDateStamp().getDayOfYear();
+//            maxTemp.add(measurements.get(i).getOutsideTemp());
+//            minTemp.add(measurements.get(i).getOutsideTemp());
         }
-        mean /= measurements.size();
-
-        System.out.println(mean);
+//        System.out.println(temperature.size());
+        int dagen = 4;
+        Graaddagen graaddagen = new Graaddagen(dagen);
+        System.out.println("Graaddagen over de afgelope "+ dagen+ " dagen: " +graaddagen.calculateGraaddagen());
+//        System.out.println("Max outside last hour: " + MSD.maximum(maxTemp));
+//        System.out.println("Min outside last hour: " + MSD.minimum(minTemp));
+//        String strDouble = String.format("%.2f", MSD.mean(temperature));
+//        System.out.println(MSD.mean(temperature));
+//        HelperFunctions.WriteOnMatrixScreen("Mean temperature past hour: " + strDouble);
     }
 }
