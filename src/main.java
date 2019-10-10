4259a972a61db5a04e4a0ae234cea0ccd0a2d956
Samirr26 <samir.rademakers2@gmail.com>
2019-10-10 17:23:32 +0100

    Degreedays Function

--- src/main.java
+++ src/main.java
@@ -1,13 +1,15 @@
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.RandomAccess;
 import java.util.concurrent.atomic.DoubleAccumulator;
 
-public class main
-{
+public class main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         IO.init();
         HelperFunctions.ClearAll();
+
+        Graaddagen ah = new Graaddagen();
+        System.out.println(  "Aantal graaddagen: "  +    ah.calculateGraaddagen(LocalDateTime.of(2010, 1,1,1,0), LocalDateTime.of(2010,12,31,22,0)));
     }
-}
+    }
