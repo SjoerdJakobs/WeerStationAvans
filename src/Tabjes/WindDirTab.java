2aa7eced7eebe3b0d62adebbfe1295f9e2d3071e
Samirr26 <samir.rademakers2@gmail.com>
2019-10-14 13:15:01 +0100

    Tabjes

---
+++ src/Tabjes/WindDirTab.java
@@ -0,0 +1,4 @@
+package Tabjes;
+
+public class WindDirTab {
+}
--- src/main.java
+++ src/main.java
@@ -1,41 +1,9 @@
-import java.io.CharArrayReader;
-//import java.time.Period;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.RandomAccess;
-import java.util.concurrent.atomic.DoubleAccumulator;
-
 public class main
 {
 
     public static void main(String[] args)
     {
-        IO.init();
-        HelperFunctions.ClearAll();
-
-
-        Calculations a = new Calculations();
-
-        ArrayList<RawMeasurement> rawValues = DatabaseConnection.getMeasurementsLastYear();
-        ArrayList<Measurement> measurements = new ArrayList<>();
-        for (int i = 0; i < rawValues.size(); i++)
-        {
-            measurements.add(new Measurement(rawValues.get(i)));
-        }
-
-        System.out.println(a.calculateDegreeDays(measurements));
-        /*Period lastYear = new Period(365);
-        ArrayList<Measurement> measurements = lastYear.getMeasurements();
-        ArrayList<Double> temps = new ArrayList<Double>();
-        System.out.println(Calculations.mist(measurements));
-        for (Measurement reading : measurements){
-            if (!Double.isNaN(reading.getOutsideTemp())){
-                temps.add(reading.getOutsideTemp());
-            }
-        }
-        System.out.println(Calculations.mean(temps));
-        System.out.println(Calculations.maximum(temps));
-        System.out.println(Calculations.minimum(temps));
-        System.out.println(Calculations.median(temps));*/
+        weerstation.initialise();
+        weerstation.menu();
     }
 }
