fd1f378d7a621c0c72ad3c3db2fc63e599fcb3d7
kruimeldief <d.iemeel@miracom.nl>
2019-10-17 13:12:16 +0200

    Showing graph almost bug free. Show current value is still experimental.

--- src/ExampleTab2.java
+++ src/ExampleTab2.java
@@ -7,14 +7,59 @@ public class ExampleTab2 extends Tab
         super(menu);
     }
 
+    // Get raw measurements of period
+    private Period period = new Period(LocalDate.of(2010, 10, 20), LocalDate.of(2010, 11, 30));
+
+    // Get measurements from period
+    private ArrayList<Measurement> measurements = period.getDataStorage().getPeriodMeasurements();
+
+    // In this TAB, we want all the outside temperatures
+    ArrayList<Double> unitValues = GetUnit(measurements);
+
+    // Create a PixelGrid to keep track of the shown dots
+    PixelGrid graph = new PixelGrid();
+
+    // MENU VARIABLES
+    private int menuCounter = 0;
+    private boolean showGraph = false;
+
+    // DATA VARIABLES
+    private double currentUnitValue;
+    private double minUnitValue = period.getDataStorage().getMinOutsideTemp();
+    private double maxUnitValue = period.getDataStorage().getMaxOutsideTemp();;
+    private double averageUnitValue = period.getDataStorage().getMeanOutsideTemp();;
+    private double ModeUnitValue = period.getDataStorage().getModeOutsideTemp();;
+    private double MedianUnitValue = period.getDataStorage().getMedianOutsideTemp();;
+    private double stdDevUnitValue = period.getDataStorage().getStandardDeviationOutsideTemp();;
+
+    // GRAPH VARIABLES
+    private int topBoundary = 8;
+    private int rightBoundary = 127;
+    private int bottomBoundary = 29;
+    private int leftBoundary = 0;
+    private int yAxel = 20; // The position on the x-axel for the vertical (y-)axel
+    private double minValue = Calculations.minimum(unitValues);
+    private double maxValue = Calculations.maximum(unitValues);
+    private double amplitudeOfUnitValues = maxValue - minValue;
+    private int    amplitudeOfGraph = bottomBoundary - topBoundary - 1; // Minus one to not draw on the x-axes
+
+    private int xIndex, oldResult = 0, step = 50, graphRestart = 0, drawColomn = 127;
+    private double positionBar = unitValues.size() / step / (double)(rightBoundary - leftBoundary);
+
+    private double deltaTimer = 0;
+    private double graphSpeed = 0.03;
+
+    // Runs when tab is opened
     @Override
     protected void OnOpen() {
-        //runs when tab is opened
-        System.out.println("exampletab 2 opened");
         HelperFunctions.ClearTextDisplay();
         m_menu.DrawMenu();
 
-
+        // Get current temperature
+        RawMeasurement rawData = DatabaseConnection.getMostRecentMeasurement();
+        Measurement measurement = new Measurement(rawData);
+        currentUnitValue = measurement.getOutsideTemp();
+        HelperFunctions.WriteOnMatrixScreen(String.format("\nOutside Temperature\ncurrent: %.2f", currentUnitValue) + " C");
     }
 
     @Override
@@ -26,16 +71,140 @@ public class ExampleTab2 extends Tab
 
     @Override
     protected void Run(double deltaTime) {
-        //runs every frame when tab is opened
-        //System.out.println("exampletab 2 run");
+        deltaTimer += deltaTime;
+
+        if (showGraph == true && deltaTimer >= graphSpeed) {
+            deltaTimer = 0;
+
+            // Test
+            System.out.println("xIndex: " + xIndex);
+
+            // Create new PixelGrid to update the main PixelGrid each run
+            PixelGrid addToGraph = new PixelGrid();
+
+            // Shift graph to the left only if there are more than two colomns
+            // TODO First colomn: 0, second colomn: 1? Thus xIndex >= 1?
+            if (xIndex > 1) {
+                graph = GraphMaker.ShiftGraphToLeft(graph, topBoundary, rightBoundary, bottomBoundary, leftBoundary);
+                GraphMaker.RedrawYAxel(topBoundary, bottomBoundary, yAxel);
+            }
+
+            // Current xIndex must be able to get values from ArrayList temperatures
+            if (unitValues.size() > Math.round(step * (xIndex - leftBoundary + 1))) {
+                // VARIABLES
+                int result;
+                double unitValuesAverage = GraphMaker.CalculateUnitValueAverage(unitValues, xIndex, step, leftBoundary);
+
+                if (!Double.isNaN(unitValuesAverage)) {
+                    result = (int)Math.round((unitValuesAverage - minValue) / amplitudeOfUnitValues * (double)amplitudeOfGraph);
+
+                    // If first colomn, create fake oldResult
+                    if (xIndex == 0) oldResult = result;
+
+                    // Update the pixelGrid with latest result
+                    addToGraph.PixelGrid[bottomBoundary - result - 1][drawColomn] = true;
+                } // End if-statement check if value is not NaN
+                else
+                    // If value is NaN, use oldResult
+                    result = oldResult;
+
+                // Fill vertical gaps between two consecutive values if needed
+                addToGraph = GraphMaker.GraphVerticalGapFiller(addToGraph, result, oldResult, drawColomn, bottomBoundary);
+
+                // Update for next run cycle
+                oldResult = result;
+
+                // Show position bar below graph
+                positionBar = unitValues.size() / step / (double)(rightBoundary - leftBoundary);
+                for (int y = bottomBoundary + 1; y <= 31; y++) {
+                    HelperFunctions.SetDisplayPixel(true, (int)Math.ceil(xIndex / positionBar), y);
+                    HelperFunctions.SetDisplayPixel(false, (int)Math.ceil(xIndex / positionBar ) - 2, y);
+                }
+            } // End if-statement current step is within unitValue's size
+
+            // Else (if all values are shown as graph), restart the graph
+            else {
+                // TODO EXPERIMENTAL: Remove latest unitValueAverage on segments
+                if (graphRestart == 0 && xIndex - (drawColomn - yAxel) == unitValues.size() / step ) HelperFunctions.ClearAllSegmentDisplays();
+
+                // Position bar goes back to the beginning
+                for (int y = bottomBoundary + 1; y <= 31; y++) {
+                    HelperFunctions.SetDisplayPixel(true, 127 - graphRestart - 2, y);
+                    HelperFunctions.SetDisplayPixel(false, 127 - graphRestart, y);
+                }
+
+                // Create empty width before showing the graph again
+                graphRestart++;
+                if (graphRestart == 128) {
+                    xIndex = -1; // -1 + incremental = index 0
+                    graphRestart = 0;
+                }
+            } // End else
+
+            // TODO EXPERIMENTAL: Give unitValueAverage of position yAxel
+            double unitValueAverageOnYAxel;
+            if (unitValues.size() > Math.round(step * (xIndex - (drawColomn - yAxel) - leftBoundary + 1))) {
+                unitValueAverageOnYAxel = GraphMaker.CalculateUnitValueAverage(unitValues, xIndex - (drawColomn - yAxel), step, leftBoundary);
+                if (!Double.isNaN(unitValueAverageOnYAxel))
+                    HelperFunctions.WriteValueOnSegments(1, unitValueAverageOnYAxel, 1);
+            }
+
+            PixelGridDrawer.INSTANCE_DRAWER.AddDraw(addToGraph.PixelGrid);
+            xIndex++;
+
+            // Update graph: add the last colomn
+            graph.OrGrid(addToGraph);
+        } // End if-statement showGraph
 
     }
 
     @Override
     protected void OnButtonBlueTwo() {
-        //runs when red button is pressed(runs once, its an actual bu)
+        menuCounter++;
+        HelperFunctions.ClearTextDisplay();
+        m_menu.DrawMenu();
 
-        System.out.println("exampletab 2  bluebutton pressed");
+        switch (menuCounter % 8) {
+            case 0:
+                showGraph = false;
+
+                // Get current temperature
+                RawMeasurement rawData = DatabaseConnection.getMostRecentMeasurement();
+                Measurement measurement = new Measurement(rawData);
+                currentUnitValue = measurement.getOutsideTemp();
+
+                HelperFunctions.WriteOnMatrixScreen(String.format("\nOutside Temperature\ncurrent: %.2f", currentUnitValue) + " C");
+                break;
+            case 1:
+                HelperFunctions.WriteOnMatrixScreen(String.format("\nOutside Temperature\nmin: %.2f", minUnitValue) + " C");
+                break;
+            case 2:
+                HelperFunctions.WriteOnMatrixScreen(String.format("\nOutside Temperature\nmax: %.2f", maxUnitValue) + " C");
+                break;
+            case 3:
+                HelperFunctions.WriteOnMatrixScreen(String.format("\nOutside Temperature\naverage: %.2f", averageUnitValue) + " C");
+                break;
+            case 4:
+                HelperFunctions.WriteOnMatrixScreen(String.format("\nOutside Temperature\nmodus: %.2f", ModeUnitValue) + " C");
+                break;
+            case 5:
+                HelperFunctions.WriteOnMatrixScreen(String.format("\nOutside Temperature\nmedian: %.2f", MedianUnitValue) + " C");
+                break;
+            case 6:
+                HelperFunctions.WriteOnMatrixScreen(String.format("\nOutside Temperature\nstd deviation: %.2f", stdDevUnitValue));
+                break;
+            case 7:
+                showGraph = true;
+
+                // Draw axels
+                PixelGrid graph = new PixelGrid();
+                for (int i = leftBoundary; i <= rightBoundary; i++) graph.PixelGrid[bottomBoundary][i] = true;
+                //for (int i = topBoundary; i <= bottomBoundary; i++) graph.PixelGrid[i][yAxel] = true;
+                PixelGridDrawer.INSTANCE_DRAWER.AddDraw(graph.PixelGrid);
+                break;
+
+        }
+
     }
 
     @Override
@@ -44,4 +213,11 @@ public class ExampleTab2 extends Tab
 
         System.out.println("exampletab 2 redbutton pressed");
     }
+
+    private ArrayList<Double> GetUnit(ArrayList<Measurement> measurements) {
+        ArrayList<Double> data = new ArrayList<>();
+        for (int i = 0; i < measurements.size(); i++) data.add(measurements.get(i).getOutsideTemp());
+        return data;
+    }
+
 }
--- src/GraphMaker.java
+++ src/GraphMaker.java
@@ -3,23 +3,81 @@ import java.lang.Math;
 
 public class GraphMaker {
 
-    public static void createAxels(PixelGrid pixelGrid) {
-        byte topBoundary = 8;
-        byte rightBoundary = 127;
-        byte bottomBoundary = 29;
-        byte leftBoundary = 0;
-        byte yAxel = 20; // position x of yAxel
-
-        // Draw the x and y axels on the left- and bottom boundary
-
-
-        for (int i = leftBoundary; i <= rightBoundary; i++) pixelGrid.PixelGrid[bottomBoundary][i] = true;
-        for (int i = topBoundary; i <= bottomBoundary; i++) pixelGrid.PixelGrid[i][yAxel] = true;
-
-        PixelGridDrawer.INSTANCE_DRAWER.AddDraw(pixelGrid.PixelGrid);
-
-        // return pixelGrid;
+    /**
+     * Draw the axels of the graph
+     * @param topBoundary Boundary of the top for the container in which the graph is displayed
+     * @param rightBoundary Boundary of the right side for the container in which the graph is displayed
+     * @param bottomBoundary Boundary of the bottom for the container in which the graph is displayed
+     * @param leftBoundary Boundary of the left side for the container in which the graph is displayed
+     * @param yAxel The position on the x-axel for the vertical (y-)axel
+     */
+    public static void CreateAxels(int topBoundary, int rightBoundary, int bottomBoundary, int leftBoundary, int yAxel) {
+        for (int i = leftBoundary; i <= rightBoundary; i++) HelperFunctions.SetDisplayPixel(true, i, bottomBoundary);
+        for (int i = topBoundary; i <= bottomBoundary; i++) HelperFunctions.SetDisplayPixel(true, yAxel, i);
     }
+
+    /**
+     * Redraw the yAxel
+     * @param topBoundary Boundary of the top for the container in which the graph is displayed
+     * @param bottomBoundary Boundary of the bottom for the container in which the graph is displayed
+     * @param yAxel The position on the x-axel for the vertical (y-)axel
+     */
+    public static void RedrawYAxel(int topBoundary, int bottomBoundary, int yAxel) {
+        for (int i = topBoundary; i <= bottomBoundary; i++) HelperFunctions.SetDisplayPixel(true, yAxel, i);
+    }
+
+    /**
+     * Shift the graph one dot to the left
+     * @param pixelGrid 2DArray with current image on DotMatrixDisplay
+     * @return updated pixelGrid
+     */
+    public static PixelGrid ShiftGraphToLeft(PixelGrid pixelGrid, int topBoundary, int rightBoundary, int bottomBoundary, int leftBoundary) {
+        for (int x = leftBoundary; x <= rightBoundary; x++) {
+            for (int y = topBoundary; y <= bottomBoundary - 1; y++) {
+                if (pixelGrid.PixelGrid[y][x] == true) {
+                    // Shift the dots one to the left
+                    HelperFunctions.SetDisplayPixel(false, x, y);
+                    HelperFunctions.SetDisplayPixel(true, x-1, y);
+
+                    // Update the pixelGrid with updated shown graph
+                    pixelGrid.PixelGrid[y][x] = false;
+                    if (x != 0) pixelGrid.PixelGrid[y][x-1] = true;
+                }
+            }
+        }
+        return pixelGrid;
+    }
+
+    public static double CalculateUnitValueAverage(ArrayList<Double> unitValues, int xIndex, int step, int leftBoundary) {
+        double sum = 0;
+        int nValues = 0;
+
+        if (xIndex >= 0) {
+            for (int i = Math.round(step * (xIndex - leftBoundary)); i < Math.round(step * (xIndex - leftBoundary + 1)); i++) {
+                sum += unitValues.get(i);
+                if (!Double.isNaN(unitValues.get(i))) nValues++;
+            }
+            return sum / nValues;
+        }
+        else
+            return Double.NaN;
+    }
+
+    public static PixelGrid GraphVerticalGapFiller(PixelGrid pixelGrid, int result, int oldResult, int drawColomn, int bottomBoundary) {
+        if (result - oldResult > 1) {
+            for (int i = oldResult + 1; i < result; i++) {
+                pixelGrid.PixelGrid[bottomBoundary - i - 1][drawColomn] = true; // Minus one to prevent from drawing on the x-axel
+            }
+        }
+        else if (result - oldResult < -1) {
+            for (int i = oldResult - 1; i > result; i--) {
+                pixelGrid.PixelGrid[bottomBoundary - i - 1][drawColomn] = true; // Minus one to prevent from drawing on the x-axel
+            }
+        }
+        return pixelGrid;
+    }
+
+    // OLD FUNCTIONS
 
     public static PixelGrid createGraph(ArrayList<Double> measurements) {
 
@@ -81,4 +139,6 @@ public class GraphMaker {
 
         return frame;
     } // End function
+
+
 } // End class
