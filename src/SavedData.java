63221f3f9c7550395e5382186b7927746f3cad32
kruimeldief <d.iemeel@miracom.nl>
2019-10-17 21:28:07 +0200

    SetGraphStep added.

--- src/SavedData.java
+++ src/SavedData.java
@@ -6,19 +6,26 @@ public enum SavedData
 
     public Measurement LastMeasurement;
     public Period SavedPeriod;
+    public int graphStep = SetGraphStep();
 
-    public void SetPeriod(LocalDate startOfPeriod, LocalDate endOfPeriod)
-    {
+    private int SetGraphStep() {
+        int nMeasurements = SavedPeriod.getDataStorage().getPeriodMeasurements().size();
+        int nDisplayShifts = 5;
+        int graphStep = nMeasurements / (nDisplayShifts * 128); // DotMatrixDisplay has a width of 128 dots
+        if (graphStep <= 100) return graphStep;
+        else return 100;
+    }
+
+    public void SetPeriod(LocalDate startOfPeriod, LocalDate endOfPeriod) {
         SavedPeriod = new Period(startOfPeriod, endOfPeriod);
     }
 
-    public void setStepWidth(int stepWidth){
-        int newStepWidth = stepWidth; 
+    public void setStepWidth(int graphStep) {
+        this.graphStep = graphStep;
     }
 
 
-    public void SetLastMeasurement()
-    {
+    public void SetLastMeasurement() {
         LastMeasurement = new Measurement(DatabaseConnection.getMostRecentMeasurement());
     }
 }
